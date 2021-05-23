1b118381453d2dea350591a609b80d810c15bead
Inokinoki <veyx.shaw@gmail.com>
2021-05-23 17:28:54 +0200

    Add basic page structure

---
+++ index.js
@@ -0,0 +1,52 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+const path = require('path')
+
+const elf_tools = require('elf-tools')
+const fs = require('fs');
+
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  })
+
+  win.loadFile('index.html')
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  ipcMain.handle('load-file', (event, arg) => {
+      const filename = dialog.showOpenDialogSync({ properties: ['openFile'] })
+      if (filename && filename.length > 0) {
+        // read the file into a buffer
+        const elf_bytes = fs.readFileSync(filename[0])
+
+        // parse the data
+        const elf = elf_tools.parse(elf_bytes)
+        return {
+          elf: elf,
+          name: filename[0]
+        };
+      }
+  })
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
